dfdb154b02d39914c650cd724de05ac48cf7cb49
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-05-15 17:00:42 -0600

    ccc

--- server2.c
+++ server2.c
@@ -37,8 +37,8 @@ void str_trim_lf (char* arr, int length) {
   int i;
   for (i = 0; i < length; i++) { // trim \n
     if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
+      arr[i] = ' ';
+    //   break;
     }
   }
 }
