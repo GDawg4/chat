fd836ab4f7b01409a3f8d5536603e468fbe4ca4f
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-05-20 01:12:12 -0600

    c

--- client3.c
+++ client3.c
@@ -352,17 +352,17 @@ int main(int argc, char **argv)
     IPbuffer = "127.1.0.1";
     //Create User Registration
     Chat__ClientPetition cli_ptn = CHAT__CLIENT_PETITION__INIT;
-    Chat__UserRegistration *user_reg = CHAT__USER_REGISTRATION__INIT; 
+    Chat__UserRegistration user_reg = CHAT__USER_REGISTRATION__INIT; 
     void *buf;                                                          
     unsigned len;              
 
-    strcpy(user_reg->username, name);
-    strcpy(user_reg->ip, IPbuffer);
-    // user_reg.username = name;
-    // user_reg.ip = IPbuffer;
+    // strcpy(user_reg->username, name);
+    // strcpy(user_reg->ip, IPbuffer);
+    user_reg.username = name;
+    user_reg.ip = IPbuffer;
 
     cli_ptn.option = 1;
-    cli_ptn.registration = &user_reg;
+    cli_ptn.registration = user_reg;
 
     len = chat__client_petition__get_packed_size(&cli_ptn);
     buf = malloc(len);
