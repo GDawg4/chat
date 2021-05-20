0a457bd9d38f9315b69c23f4110044b38f4f5ecf
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-05-20 12:52:10 -0600

    fdfd

--- client3.c
+++ client3.c
@@ -10,10 +10,6 @@
 #include <pthread.h>
 #include <netdb.h>
 #include "new.pb-c.h"
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <ifaddrs.h>
-
 // #include "amessage.pb-c.h"
 #define MAX_MSG_SIZE 1024
 #define BUFFER_SZ 2048 * 24
@@ -333,32 +329,13 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    struct ifaddrs * ifAddrStruct=NULL;
-    struct ifaddrs * ifa=NULL;
-    void * tmpAddrPtr=NULL;
+    char ip_buffer;
+    char hostbuffer[256];
+    struct hostent *host_entry;
 
-    getifaddrs(&ifAddrStruct);
-
-    for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
-        if (!ifa->ifa_addr) {
-            continue;
-        }
-        if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
-            // is a valid IP4 Address
-            tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
-            char addressBuffer[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
-        } else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
-            // is a valid IP6 Address
-            tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
-            char addressBuffer[INET6_ADDRSTRLEN];
-            inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
-            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
-        } 
-    }
-    if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
-    // printf("IP Address is %s - %s\n" , array , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+    host_entry = gethostbyname(hostbuffer);
+    ip_buffer = "3.142.76.26";
+    
 
 
     // Send name
