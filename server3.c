ea7ef49a05e5c7cd859d21678665bb6d499c97dd
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2021-05-20 16:13:31 -0600

    d

--- server3.c
+++ server3.c
@@ -209,7 +209,7 @@ void sendFailureServerResponse(char *failure_message, client_t *client_sender, i
 }
 
 /* Change User Status*/
-void change_user_status(client_t client , char status , char *username)
+void change_user_status(client_t *client , char *status , char *username)
 {
 	printf("FUCK\n");
 	
