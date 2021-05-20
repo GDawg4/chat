0891a818a4c99e5b70fabd84a14da70912a1c397
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2021-05-20 15:59:02 -0600

    cd

--- server3.c
+++ server3.c
@@ -39,6 +39,19 @@ void str_overwrite_stdout()
 	fflush(stdout);
 }
 
+void str_trim_lf(char *arr, int length)
+{
+	int i;
+	for (i = 0; i < length; i++)
+	{ // trim \n
+		if (arr[i] == "\n")
+		{
+			arr[i] = " ";
+			//   break;
+		}
+	}
+}
+
 // char* get_ip(struct sockaddr_in addr)
 // {
 
