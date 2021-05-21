eb31b3b4a6cb4e9dbd7b1c799d997f5ce539293b
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-05-20 20:47:44 -0600

    dfd

--- server3.c
+++ server3.c
@@ -245,13 +245,13 @@ void get_user_list(client_t *client)
 	unsigned len;
 	srv_res.option = 2;
 	users->n_connectedusers = cli_count;
-	users->connectedusers=&connectedClients;
-	srv_res.connectedusers = &users;
-	srv_res.code = 200;
-	len = chat__server_response__get_packed_size(&srv_res);
-	buf = malloc(len);
-	chat__server_response__pack(&srv_res, buf);
-	send(client->sockfd, buf, len, 0);
+	users->connectedusers= connectedClients;
+	// srv_res.connectedusers = &users;
+	// srv_res.code = 200;
+	// len = chat__server_response__get_packed_size(&srv_res);
+	// buf = malloc(len);
+	// chat__server_response__pack(&srv_res, buf);
+	// send(client->sockfd, buf, len, 0);
 	pthread_mutex_unlock(&clients_mutex);
 	free(buf);
 }
