5ab6818ca77f120b21ae4fba1b37ab0fa1b230a5
sorozcov <42949349+sorozcov@users.noreply.github.com>
2021-05-20 20:42:32 -0600

    dfdf

--- server3.c
+++ server3.c
@@ -239,20 +239,20 @@ void get_user_list(client_t *client)
 			
 		}
 	}
-	printf("LLego aqui\n");
-	// void *buf; // Buffer to store serialized data
-	// unsigned len;
-	// srv_res.option = 2;
-	// users->n_connectedusers = cli_count;
-	// users->connectedusers=connectedClients;
-	// srv_res.connectedusers = &users;
-	// srv_res.code = 200;
-	// len = chat__server_response__get_packed_size(&srv_res);
-	// buf = malloc(len);
-	// chat__server_response__pack(&srv_res, buf);
-	// send(client->sockfd, buf, len, 0);
-	// pthread_mutex_unlock(&clients_mutex);
-	// free(buf);
+
+	void *buf; // Buffer to store serialized data
+	unsigned len;
+	srv_res.option = 2;
+	users->n_connectedusers = cli_count;
+	users->connectedusers=connectedClients;
+	srv_res.connectedusers = &users;
+	srv_res.code = 200;
+	len = chat__server_response__get_packed_size(&srv_res);
+	buf = malloc(len);
+	chat__server_response__pack(&srv_res, buf);
+	send(client->sockfd, buf, len, 0);
+	pthread_mutex_unlock(&clients_mutex);
+	free(buf);
 }
 
 /* Get User Information Request*/
